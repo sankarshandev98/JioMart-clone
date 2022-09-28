1e8d4b25f81915ba5a2ef8e1c102c6890a747e39
sankarshandev98 <gsankar164@gmail.com>
2022-09-28 21:54:02 +0530

    update

--- jio-mart/src/Component/Searchbar.js
+++ jio-mart/src/Component/Searchbar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import nav from "./Navbar.module.css";
+import * as faIcons from 'react-icons/fa';
 
 function Searchbar() {
     return (
         <div>
             <input className={nav.Searchbar}
                 type="search"
-                placeholder="Search essentials, groceries, and more ..." />
+                placeholder="Search essentials, groceries, and more ..." / ><span>
+                <faIcons.FaListUl className={nav.listui}/>
+            </span>
         </div>
     )
 }
