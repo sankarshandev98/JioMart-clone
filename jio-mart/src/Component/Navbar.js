86d3e1e645b01beb408b18e7d6e52174310ba9a0
sankarshandev98 <gsankar164@gmail.com>
2022-09-28 19:45:57 +0530

    update

--- jio-mart/src/Component/Navbar.js
+++ jio-mart/src/Component/Navbar.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
+import * as AiIcons from 'react-icons/ai';
+import { SidebarData } from './SidebarData';
 
 const Navbar = () => {
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => {
+        setSidebar(!sidebar);
+    }
     return (
-        <div>
-            <Link to="/"><h2>Home</h2></Link>
-        </div>
+        <>
+            <div className='navbar'>
+                <Link to="/" className='menu-bars'>
+                    <AiIcons.AiOutlineMenu onClick={showSidebar} />
+                </Link>
+            </div>
+            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+                <ul className='nav-menu-items'>
+                    <li className='navbar-toggle'>
+                        <Link to="#" className='menu-bars'>
+                            <AiIcons.AiOutlineClose />
+                        </Link>
+                    </li>
+                    {SidebarData.map((item, index) => {
+                        return (
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.title}
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </>
     )
 }
+
+// 13.31
 
 export default Navbar;
