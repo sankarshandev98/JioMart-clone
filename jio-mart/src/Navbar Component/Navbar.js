cbf0174579ee732f7daf143ec44868c23387ab1f
sankarshandev98 <gsankar164@gmail.com>
2022-09-29 06:12:05 +0530

    update

--- jio-mart/src/Navbar Component/Navbar.js
+++ jio-mart/src/Navbar Component/Navbar.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import * as AiIcons from 'react-icons/ai';
 import * as faIcons from 'react-icons/fa';
 import { SidebarData } from './SidebarData';
-import nav from "./Navbar.module.css";
+import styles from "./Navbar.module.css";
 import Searchbar from './Searchbar';
 import Login_signup from './Login_signup';
 import Cart from './Cart';
-
+import "./Nav.css";
 
 const Navbar = () => {
     const [sidebar, setSidebar] = useState(false);
@@ -16,19 +16,19 @@ const Navbar = () => {
     }
     return (
         <>
-            <div className={nav.navbar}>
-                <Link to="/" className='menu-bars'>
-                    <AiIcons.AiOutlineMenu className={nav.nav_toggle} onClick={showSidebar} />
+            <div className={styles.navbar}>
+                <Link to="#" className={styles.menu_bars}>
+                    <AiIcons.AiOutlineMenu onClick={showSidebar} />
                 </Link>
-                <img className={nav.main_logo} src='https://www.jiomart.com/assets/version1662994539/smartweb/images/jiomart_logo_beta.svg' />
+                <img className={styles.main_logo} src='https://www.jiomart.com/assets/version1662994539/smartweb/images/jiomart_logo_beta.svg' />
                 <Searchbar />
                 <Login_signup />
                 <Cart />
             </div>
             <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-                <ul className='nav-menu-items'>
-                    <li className='navbar-toggle'>
-                        <Link to="#" className='menu-bars'>
+                <ul className={styles.nav_menu_items} onClick={showSidebar}>
+                    <li className={styles.navbar_toggle}>
+                        <Link to="#" className={styles.menu_bars}>
                             <AiIcons.AiOutlineClose />
                         </Link>
                     </li>
