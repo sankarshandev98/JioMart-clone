be69a92e933d68f5829ce9dbf5b6d71c3c362232
sankarshandev98 <gsankar164@gmail.com>
2022-09-30 10:55:33 +0530

    update

--- jio-mart/src/Dropdown Component/Nav.js
+++ jio-mart/src/Dropdown Component/Nav.js
@@ -1,10 +1,15 @@
 import { menuItemsData } from "./menuItemsData";
 import MenuItems from "./MenuItems";
 import "./Dropdown.css";
+import * as faIcons from 'react-icons/fa';
 const Nav = () => {
     return (
         <nav>
             <ul className="menus">
+                <div className="menus-deliver">
+                    <faIcons.FaMapMarkerAlt className="location-logo" />
+                    <p>Deliver to 400020</p>
+                </div>
                 {
                     menuItemsData.map((menu, index) => {
                         const depthLevel = 0;
@@ -12,7 +17,7 @@ const Nav = () => {
                     })
                 }
             </ul>
-        </nav>
+        </nav >
     );
 };
 
