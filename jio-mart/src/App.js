e5c78b2e32935e75dd2eb324fb3375ee8f1a16ed
sankarshandev98 <gsankar164@gmail.com>
2022-10-01 20:06:38 +0530

    update

--- jio-mart/src/App.js
+++ jio-mart/src/App.js
@@ -3,11 +3,15 @@ import AllRoutes from "./Pages/AllRoutes";
 import Navbar from "./Component/Navbar Component/Navbar";
 import Footer1 from "./Component/Footer/Footer";
 import LandingPage from "./Pages/landingPage/LandingPage";
+import ProductLayout from "./Component/ProductLayout";
+
+
 function App() {
   return <div className="App">
     <Navbar />
     <AllRoutes />
     <LandingPage />
+    <ProductLayout />
     <Footer1 />
   </div>
 }
