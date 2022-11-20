282502832c6fc6b0ac47173878ed454b6cff77a7
sankarshandev98 <gsankar164@gmail.com>
2022-11-20 20:36:47 +0530

    update

--- jio-mart/src/Pages/ProductsPage.jsx
+++ jio-mart/src/Pages/ProductsPage.jsx
@@ -7,60 +7,62 @@ import "./styles.css";
 import RingLoader from "react-spinners/RingLoader";
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+  const {
+    state: { products },
+    productState: { sort, byStock, searchQuery },
+  } = CartState();
 
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false)
-        }, 2000)
-    }, [])
-    const {
-        state: { products },
-        productState: { sort, byStock, searchQuery },
-    } = CartState();
+  const transformProducts = () => {
+    let sortedProducts = products;
 
-    const transformProducts = () => {
-        let sortedProducts = products;
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
 
-        if (sort) {
-            sortedProducts = sortedProducts.sort((a, b) =>
-                sort === "lowToHigh" ? a.price - b.price : b.price - a.price
-            );
-        }
+    if (byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock);
+    }
 
-        if (byStock) {
-            sortedProducts = sortedProducts.filter((prod) => prod.inStock);
-        }
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.name.toLowerCase().includes(searchQuery)
+      );
+    }
 
-
-        if (searchQuery) {
-            sortedProducts = sortedProducts.filter((prod) =>
-                prod.name.toLowerCase().includes(searchQuery)
-            );
-        }
-
-        return sortedProducts;
-    };
-    return (
-        <div className="home">
-            {loading ? (
-                <RingLoader className="spin"
-                    color={"#008ECC"} loading={loading} size={150} />
-            )
-                : (
-                    <>
-                        <Filters />
-                        <div className="productContainer">
-                            {transformProducts().map((prod) => (
-                                <SingleProduct prod={prod} key={prod.id} />
-                            ))}
-                        </div>
-                    </>
-                )
-            }
-        </div>
-    );
+    return sortedProducts;
+  };
+  return (
+    <div className="home">
+      {loading ? (
+        <RingLoader
+          className="spin"
+          color={"#008ECC"}
+          loading={loading}
+          size={150}
+        />
+      ) : (
+        <>
+          <div>
+            <Filters />
+          </div>
+          <div className="productContainer">
+            {transformProducts().map((prod) => (
+              <SingleProduct prod={prod} key={prod.id} />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
